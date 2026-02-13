921b5dcc2c57736717c6fe449e194bf785ab424f
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-13 18:00:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send("Server is awake!"));
+
+app.post('/save', (req, res) => {
+    fs.writeFileSync('./data.json', JSON.stringify(req.body));
+    res.json({ status: "Success" });
+});
+
+app.get('/load', (req, res) => {
+    if (fs.existsSync('./data.json')) {
+        res.send(fs.readFileSync('./data.json'));
+    } else {
+        res.json({ content: "" });
+    }
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
