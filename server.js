04275865ba3c016b1b2f5187ad8c5a8c651faeac
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-25 14:46:41 +0530

    trying sdata again

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send("Server is awake!"));
 
-app.post('/save', (req, res) => {
-    writeFileSync('./data.json', JSON.stringify(req.body));
+app.post('/savesdata1', (req, res) => {
+    writeFileSync('./sdata1.json', JSON.stringify(req.body));
     res.json({ status: "Success" });
 });
 
-app.get('/load', (req, res) => {
-    if (existsSync('./data.json')) {
-        const fileData = readFileSync('./data.json', 'utf8');
+app.get('/loadsdata1', (req, res) => {
+    if (existsSync('./sdata1.json')) {
+        const fileData = readFileSync('./sdata1.json', 'utf8');
         res.json(JSON.parse(fileData)); // This ensures it's sent as a clean JSON object
     } else {
         res.json({ message: "" });
